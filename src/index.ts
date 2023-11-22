1bfb5072f2d0a10dff554e5c7b44aefbf840b7d9
Mitchell Sayre <sayremitch@gmail.com>
2023-11-22 00:46:00 -0500

    Download archive from AIHub google sheet

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,18 @@ import https from "https";
 import fs from "fs";
 import "dotenv/config";
 
-import { testing } from "./util/parse-google-sheet.js";
+const docId = process.env.DOC_ID as string;
+const sheetId = process.env.SHEET_ID as string;
+const format = "json";
+const url = `https://docs.google.com/spreadsheets/d/${docId}/gviz/tq?tqx=out:${format}&tq&gid=${sheetId}`;
 
-const realtimeArchiveUrl = process.env.REALTIME_ARCHIVE_URL as string;
-// console.log(realtimeArchiveUrl);
-// let ans = await fetchGoogleSheet(realtimeArchiveUrl);
-let ans = testing();
-// console.log(ans);
+console.log(`Downloading Google sheet at ${url}...`);
+
+const startToRemove = "/*O_o*/\ngoogle.visualization.Query.setResponse(";
+const endToRemove = ");";
+
+let response = await fetch(url);
+let data = await response.text();
+data = data.substring(startToRemove.length);
+data = data.slice(0, -endToRemove.length);
+fs.writeFileSync("./sheet.json", data);
