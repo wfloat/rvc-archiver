935df1cf0bbfac30d32393effc959633d99d6dd6
Mitchell Sayre <sayremitch@gmail.com>
2024-02-10 23:27:39 -0500

    Create shared zip of model weights using only VoiceModels that are not hidden

---
+++ src/shared_zip.py
@@ -0,0 +1,77 @@
+import os
+import zipfile
+from dotenv import load_dotenv
+from sgqlc.endpoint.http import HTTPEndpoint
+from sgqlc.operation import Operation
+from graphql.generated.schema import CreateAIHubVoiceModelInput
+from graphql.generated.operations import Operations
+from typing import Optional, List, Dict, Any
+from dataclasses import dataclass
+import zipfile
+
+# Define the array of objects with criteria for including files in the ZIP
+voice_models = []
+
+
+def file_should_be_included(folder, filename):
+    if folder == "weights" or folder == "logs":
+        filename_without_extension, extension = os.path.splitext(filename)
+        for voice_model in voice_models:
+            if voice_model["checksumSHA256ForWeights"] == filename_without_extension:
+                return True
+    return False
+
+
+specific_files_to_include = [
+    "shared/f0/f0G40k.pth",
+    "shared/f0/f0G48k.pth",
+]
+
+WFLOAT_API_URL = os.getenv("WFLOAT_API_URL")
+
+
+endpoint = HTTPEndpoint(WFLOAT_API_URL)
+
+
+def get_voice_models():
+    voice_models_query = Operations.query.voice_models
+    page_end_cursor = None
+    has_next_page = True
+
+    voice_models = []
+
+    while has_next_page:
+        res = endpoint(query=voice_models_query, variables={"after": page_end_cursor})
+
+        errors = res.get("errors")
+        if errors:
+            continue
+
+        voice_model_connection = res["data"]["VoiceModels"]
+        page_end_cursor = voice_model_connection["pageInfo"]["endCursor"]
+        has_next_page = voice_model_connection["pageInfo"]["hasNextPage"]
+
+        for edge in voice_model_connection["edges"]:
+            if edge["node"]["hidden"] == False:
+                voice_models.append(edge["node"])
+
+    page_end_cursor = None
+    has_next_page = True
+
+    return voice_models
+
+
+# Create a ZIP file
+with zipfile.ZipFile("shared.zip", "w") as myzip:
+    get_voice_models()
+
+    myzip.writestr("shared/input/", "")
+    myzip.writestr("shared/output/", "")
+
+    for folder_name, subfolders, filenames in os.walk("shared"):
+        for filename in filenames:
+            if file_should_be_included(os.path.basename(folder_name), filename):
+                file_path = os.path.join(folder_name, filename)
+                myzip.write(file_path, arcname=os.path.relpath(file_path, "shared"))
+
+print("ZIP file created successfully.")
