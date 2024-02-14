24587fac85a698ce3611c08f955f69cfcb22751e
Mitchell Sayre <sayremitch@gmail.com>
2024-02-13 22:02:58 -0500

    Add simple CLI for taking in VoiceModel id and video start and end timestamp

--- src/average_frequency.py
+++ src/average_frequency.py
@@ -7,6 +7,7 @@ import cv2
 import random
 import os
 import math
+import sys
 
 SILENCE_THRESHOLD = 10  # in hundredths of a second
 SEGMENT_MINIMUM_LENGTH = 100  # in hundredths of a second
@@ -19,6 +20,34 @@ SEGMENT_RANDOM_SELECTION_COUNT = 20
 class SpeechSegment:
     index_start: int
     index_end: int
+
+
+def duration_str_to_seconds(duration_str: str) -> int:
+    """
+    Converts a length string in the format "HH:MM:SS" or "MM:SS" to the total number of seconds.
+
+    Args:
+    duration_str (str): A string representing a time duration in the format "HH:MM:SS" or "MM:SS".
+
+    Returns:
+    int: Total number of seconds represented by the input string.
+    """
+    # Split the string into its components
+    parts = duration_str.split(":")
+
+    # Check the number of parts and calculate the total seconds accordingly
+    if len(parts) == 3:
+        hours, minutes, seconds = map(int, parts)
+        total_seconds = hours * 3600 + minutes * 60 + seconds
+    elif len(parts) == 2:
+        minutes, seconds = map(int, parts)
+        total_seconds = minutes * 60 + seconds
+    else:
+        raise ValueError(
+            "Invalid time duration format. Expected 'HH:MM:SS' or 'MM:SS'."
+        )
+
+    return total_seconds
 
 
 def read_file_to_numpy_array(file_path: str) -> List[float]:
@@ -102,7 +131,28 @@ def main():
     female_f0 = compute_f0(FEMALE_OUTPUT_DIR)
     male_f0 = compute_f0(MALE_OUTPUT_DIR)
 
-    pitch_shift = calculate_semitone_shift(female_f0, male_f0)
+    if len(sys.argv) != 4:
+        print("Usage: script.py <VoiceModelId> <start time> <end time>")
+        sys.exit(1)
+
+    voice_model_id = sys.argv[1]
+    start_time_str = sys.argv[2]
+    end_time_str = sys.argv[3]
+
+    try:
+        start_time_seconds = duration_str_to_seconds(start_time_str)
+        end_time_seconds = duration_str_to_seconds(end_time_str)
+        # Now you have the voice model ID, and start and end times in seconds
+        print(
+            f"Voice Model ID: {voice_model_id}, Start Time (seconds): {start_time_seconds}, End Time (seconds): {end_time_seconds}"
+        )
+        # Insert your next steps here
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
+
+    # pitch_shift = calculate_semitone_shift(female_f0, male_f0)
+
     print()
 
 
