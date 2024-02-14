5d43e9ffca5c24c9449ac93a5a586bb02bf00d5d
Mitchell Sayre <sayremitch@gmail.com>
2024-02-13 22:59:58 -0500

    Compute F0 for a VoiceModel from an audio segment

--- src/average_frequency.py
+++ src/average_frequency.py
@@ -8,6 +8,11 @@ import random
 import os
 import math
 import sys
+from sgqlc.endpoint.http import HTTPEndpoint
+from graphql.generated.operations import Operations
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SILENCE_THRESHOLD = 10  # in hundredths of a second
 SEGMENT_MINIMUM_LENGTH = 100  # in hundredths of a second
@@ -15,11 +20,48 @@ SEGMENT_FRAMES_SAMPLING_WIDTH = 75  # in hundredths of a second
 SEGMENT_FRAMES_COUNT = 4  # Amount of frames to pull from the video for each sample
 SEGMENT_RANDOM_SELECTION_COUNT = 20
 
+F0_SAMPLE_RATE = 100  # HZ
+
+WFLOAT_API_URL = os.getenv("WFLOAT_API_URL")
+WFLOAT_API_KEY = os.getenv("WFLOAT_API_KEY")
+
+headers = {
+    "authorization": WFLOAT_API_KEY,
+}
+endpoint = HTTPEndpoint(WFLOAT_API_URL, headers)
+
 
 @dataclass
 class SpeechSegment:
     index_start: int
     index_end: int
+
+
+def get_voice_models():
+    voice_models = []
+    voice_models_query = Operations.query.voice_models
+    page_end_cursor = None
+    has_next_page = True
+
+    while has_next_page:
+        res = endpoint(query=voice_models_query, variables={"after": page_end_cursor})
+
+        errors = res.get("errors")
+        if errors:
+            continue
+
+        voice_model_connection = res["data"]["VoiceModels"]
+        page_end_cursor = voice_model_connection["pageInfo"]["endCursor"]
+        has_next_page = voice_model_connection["pageInfo"]["hasNextPage"]
+
+        for edge in voice_model_connection["edges"]:
+            if edge["node"]["hidden"] == False:
+                voice_models.append(edge["node"])
+
+    page_end_cursor = None
+    has_next_page = True
+
+    return voice_models
 
 
 def duration_str_to_seconds(duration_str: str) -> int:
@@ -113,7 +155,15 @@ def compute_f0(input_dir: str):
         averages.append(average)
     f0 = np.mean(averages)
     return f0
-    print(f0)
+
+
+def compute_f0_from_segment(input_path: str, segment_start_time, segment_end_time):
+    frequencies = read_file_to_numpy_array(input_path)
+    start_index = segment_start_time * F0_SAMPLE_RATE
+    end_index = segment_end_time * F0_SAMPLE_RATE
+    segment_frequencies = frequencies[int(start_index) : int(end_index)]
+    average = average_excluding_below_10(segment_frequencies)
+    return average
 
 
 def calculate_semitone_shift(f0_a, f0_b):
@@ -136,22 +186,46 @@ def main():
         sys.exit(1)
 
     voice_model_id = sys.argv[1]
+    print(voice_model_id)
     start_time_str = sys.argv[2]
     end_time_str = sys.argv[3]
+
+    voice_models = get_voice_models()
+    voice_model_matches = [obj for obj in voice_models if obj["id"] == voice_model_id]
+    voice_model = voice_model_matches[0] if voice_model_matches else None
 
     try:
         start_time_seconds = duration_str_to_seconds(start_time_str)
         end_time_seconds = duration_str_to_seconds(end_time_str)
-        # Now you have the voice model ID, and start and end times in seconds
         print(
-            f"Voice Model ID: {voice_model_id}, Start Time (seconds): {start_time_seconds}, End Time (seconds): {end_time_seconds}"
+            f"start time (sec): {start_time_seconds}, end time (sec): {end_time_seconds}"
         )
-        # Insert your next steps here
+
+        gender = voice_model["sourceModel"]["inferredProfile"]["gender"]
+        f0_base = None
+        if gender == "female":
+            f0_base = female_f0
+        else:
+            f0_base = male_f0
+
+        print(
+            f"Voice Model id: {voice_model_id}, Start Time (seconds): {start_time_seconds}, End Time (seconds): {end_time_seconds}"
+        )
+
+        F0_SAMPLE_DIR = "frequency/f0"
+        f0_voice_model = compute_f0_from_segment(
+            f"{F0_SAMPLE_DIR}/{voice_model_id}.txt",
+            start_time_seconds,
+            end_time_seconds,
+        )
+
+        print(f"base f0: {f0_base}, new f0: {f0_voice_model}")
+        pitch_shift = calculate_semitone_shift(f0_base, f0_voice_model)
+        print(pitch_shift)
+
     except ValueError as e:
         print(e)
         sys.exit(1)
-
-    # pitch_shift = calculate_semitone_shift(female_f0, male_f0)
 
     print()
 
